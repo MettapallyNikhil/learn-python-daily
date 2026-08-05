c36caa10f3430c53a0213a4d508a5e1eb178fc89
Nikhil kumar Mettapally <nikhil.mettapally@gmail.com>
2026-08-05 16:07:13 +0200

    Add files via upload

---
+++ Demo.py
@@ -0,0 +1,10 @@
+print("Hello, World!")
+a = 5
+b = 10
+print('sum is:', a + b)
+#Single line comment is # and multi line comment is ''' ''' or """ """
+#Comments
+'''If we want dont want to execute the code below we can use comments'''
+#keywords or reserved words - we need to use these words as it is, as it is an Case sensitive language
+import keyword
+print(keyword.kwlist)
