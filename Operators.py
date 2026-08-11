b80cfe262db7f0cf97837de7c72049d4847688c7
Nikhil kumar Mettapally <nikhil.mettapally@gmail.com>
2026-08-11 21:28:36 +0200

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,32 @@
+# Operator is a symbol that performs an operation on one or more operands (values or variables).
+
+# 1. Arithmetic Operators: mathematical operations like addition, subtraction, multiplication,division, etc.
+# - Subtract
+# * Multiply
+# / Divide
+# % Modulus
+# ** Power
+# // Floor Division
+
+# 2. relational Operators: used to compare two values and return a boolean result (True or False).
+# < - less than
+# > - greater than
+# <= - less than or equal to
+# >= - greater than or equal to 
+# == -  equal to
+# != - not equal to
+
+# 3. Assignment Operators:  used to assign values to variables. 
+#  =    - Assign
+# +=    - Add and assign
+# -=    - Subtract and assign
+# *=    - Multiply and assign
+# /=    - Divide and assign
+# %=    - Modulus and assign
+# **=   - Power and assign
+# //=   - Floor Division and assign
+
+# 4. Logical Operators:  used to combine conditional statements. 
+# 5. membership Operators:  used to test whether a value is present in a sequence (like a list, tuple, or string). 
+# 6. Identity Operators:  used to compare the memory locations of two objects. 
+# 7. bitwise Operators:  used to perform bit-level operations on binary numbers. 
