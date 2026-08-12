f6a929c4d61a411ff560fe010c65d59429258b76
Nikhil kumar Mettapally <nikhil.mettapally@gmail.com>
2026-08-12 16:13:39 +0200

    Add files via upload

--- Operators.py
+++ Operators.py
@@ -27,6 +27,23 @@
 # //=   - Floor Division and assign
 
 # 4. Logical Operators:  used to combine conditional statements. 
+# and - Returns True if both statements are true
+# or - Returns True if one of the statements is true
+# not - Reverse the result, returns False if the result is true
+
 # 5. membership Operators:  used to test whether a value is present in a sequence (like a list, tuple, or string). 
-# 6. Identity Operators:  used to compare the memory locations of two objects. 
+# in    -  Returns True if a sequence with the specified value is present in the object
+# not in - Returns True if a sequence with the specified value is not present in the object
+
+# 6. Identity Operators:  used to compare the variables or any objects are having same identity or not. 
+# is     -  Returns True if both variables are the same identity
+# is not -  Returns True if both variables are not the same identity
+# id() -  built in function, used to find the memory address of the object, it returns a unique integer for the object during its lifetime.
+
 # 7. bitwise Operators:  used to perform bit-level operations on binary numbers. 
+# & -  AND 
+# | -  OR
+# ^ -  XOR
+# ~ -  NOT
+# << -  Left Shift
+# >> -  Right Shift
