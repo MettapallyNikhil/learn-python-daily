15cc7f62d13b80b2a58519c639a15d26fb912e4e
Nikhil kumar Mettapally <nikhil.mettapally@gmail.com>
2026-08-14 22:19:40 +0200

    Add files via upload

--- Control Flow Statements.py
+++ Control Flow Statements.py
@@ -6,11 +6,13 @@
 # nested if: A if block or else block having one more if else within it, it is used to test multiple blocks
 # elif: The short form Else If
 
-
-
 # iterative statements
-# for
-# while
+# for - to print one by one values - to iterate the elemants of collection what the order they appear
+# nest for loop - a for loop which is havin once more for loop within it
+# note: for every iteration of outer loop, inner loop should finish its all iterations then only outer loop with 
+# next iterations
+# uses - 2d concepts, rows and columns, limited interation we use for loop
+# while - unlimited iterations, till the condition is false it keeps on executing
 
 # Trasfer statements
 # Break
