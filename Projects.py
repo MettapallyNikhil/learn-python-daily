f9824a22cdaf4f2147f0ae8024044d071ab02bc6
Nikhil kumar Mettapally <nikhil.mettapally@gmail.com>
2026-08-26 12:22:11 +0200

    Number Guessing Game

---
+++ Projects.py
@@ -0,0 +1,21 @@
+# Number Guessing Game
+secret_number = 22
+attempts = 0
+while True:
+    # Ask for user's guess
+    try:
+        guess = int(input("Enter a number: "))
+    except ValueError:
+        print("That's not a valid number.")
+        continue
+    # Increase attempts
+    attempts += 1
+    # Check the guess
+    if guess > secret_number:
+        print("Too high")
+    elif guess < secret_number:
+        print("Too Low")
+    else:
+        print("Correct")
+        break
+print(attempts)
