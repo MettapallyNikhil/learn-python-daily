46d51dac17098e72603c7c5463ecefc7513f51d2
Nikhil kumar Mettapally <nikhil.mettapally@gmail.com>
2026-09-02 16:58:13 +0200

    Add files via upload

--- Think Python How to Think Like a Computer Scientist.py
+++ Think Python How to Think Like a Computer Scientist.py
@@ -90,9 +90,16 @@
 r = 5
 volume = (4/3)*3.14159*(r**3)
 print(volume)
+
 # 2. Suppose the cover price of a book is $24.95, but bookstores get a 40% discount. Shipping costs
 # $3 for the first copy and 75 cents for each additional copy. What is the total wholesale cost for 60 copies?
-
+cover_price_of_a_book = 24.95
+discount = 0.4
+shipping_costs_for_the_first_copy = 3
+shipping_costs_for_additional_copies = 0.75
+wholesale_cost = cover_price_of_a_book * (1 - discount) * 60 + shipping_costs_for_the_first_copy 
++ shipping_costs_for_additional_copies * (60 - 1)
+print(wholesale_cost)
 
 # 3. If I leave my house at 6:52 am and run 1 mile at an easy pace (8:15 per mile), then 3 miles at
 # tempo (7:12 per mile) and 1 mile at easy pace again, what time do I get home for breakfast?
