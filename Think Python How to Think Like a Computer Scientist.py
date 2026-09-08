fa8ec87de3bb36db167770e5f76c806ebd44ad90
Nikhil kumar Mettapally <nikhil.mettapally@gmail.com>
2026-09-08 22:59:32 +0200

    Exercise 3.2 completed

--- Think Python How to Think Like a Computer Scientist.py
+++ Think Python How to Think Like a Computer Scientist.py
@@ -126,8 +126,8 @@
 #     print(' ' * (70 - len(s)) + s)
 # right_justify('monty')
 
-import math
-math 
+# import math
+# math.sqrt(10)
 
 # def print_lyrics():
 #     print("I'm a lumberjack, and I'm okay.")
@@ -175,25 +175,35 @@ math
 
 # A function object is a value you can assign to a variable or pass as an argument. 
 # For example, do_twice is a function that takes a function object as an argument and calls it twice:
-def do_twice(f):
-    f()
-    f()
+# def do_twice(f):
+#     f() #function object
+#     f()
 # Here’s an example that uses do_twice to call a function named print_spam twice.
-def print_spam():
-    print('spam')
+# def print_spam():
+#     print('spam')
     
-do_twice(print_spam)
-
+# do_twice(print_spam)
+ 
 # 1. Type this example into a script and test it.
-do_twice(print_spam)
+# do_twice(print_spam)
 
 # 2. Modify do_twice so that it takes two arguments, a function object and a value, and calls the
 # function twice, passing the value as an argument.
-
+def do_twice(f, value):
+    f(value) #function object
+    f(value)
 
 # 3. Copy the definition of print_twice from earlier in this chapter to your script.
-# 4. Use the modified version of do_twice to call print_twice twice, passing 'spam' as an
-# argument.
+def print_twice(bruce):
+    print(bruce)
+    print(bruce)
+
+# 4. Use the modified version of do_twice to call print_twice twice, passing 'spam' as an argument.
+do_twice(print_twice, 'spam')
+
 # 5. Define a new function called do_four that takes a function object and a value and calls the
 # function four times, passing the value as a parameter. There should be only two statements in
 # the body of this function, not four.
+def do_four(f, value):
+    do_twice(f, value)
+    do_twice(f, value)
