58aef014e3f8c34a147e00dde2d6f99f8e86ed06
Nikhil kumar Mettapally <nikhil.mettapally@gmail.com>
2026-09-10 16:55:39 +0200

    Exercise.4.2 - The turtle module

---
+++ mypolygon.py
@@ -0,0 +1,14 @@
+# Case study: interface design
+4.1 The turtle module
+import turtle
+bob = turtle.Turtle()
+bob.fd(100)
+bob.lt(90)
+bob.fd(100)
+bob.lt(90)
+bob.fd(100)
+bob.lt(90)
+bob.fd(100)
+bob.lt(90)
+print(bob)
+turtle.mainloop()
