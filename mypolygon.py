22764995898bbea5a00882044e826ab147e8f1b4
Nikhil kumar Mettapally <nikhil.mettapally@gmail.com>
2026-09-11 15:54:44 +0200

    Exercise 4.2

--- mypolygon.py
+++ mypolygon.py
@@ -1,14 +1,29 @@
 # Case study: interface design
-4.1 The turtle module
+# 4.1 The turtle module
+# import turtle
+# bob = turtle.Turtle()
+
+# bob.fd(100)
+# bob.lt(90)
+# bob.fd(100)
+# bob.lt(90)
+# bob.fd(100)
+# bob.lt(90)
+# bob.fd(100)
+# bob.lt(90)
+# print(bob)
+# turtle.mainloop()
+
+# 4.2 Simple repetition 
+
+# for i in range(4):
+#     print('Hello!')
+
+# Drawing a square using a for loop
+
 import turtle
 bob = turtle.Turtle()
-bob.fd(100)
-bob.lt(90)
-bob.fd(100)
-bob.lt(90)
-bob.fd(100)
-bob.lt(90)
-bob.fd(100)
-bob.lt(90)
-print(bob)
+for i in range(4):
+    bob.fd(100)
+    bob.lt(90)
 turtle.mainloop()
