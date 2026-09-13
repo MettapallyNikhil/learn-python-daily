d8a9eab0ad4dee3f4b265f946909457c30076b0a
Nikhil kumar Mettapally <nikhil.mettapally@gmail.com>
2026-09-13 14:46:23 +0200

    Add files via upload

--- mypolygon.py
+++ mypolygon.py
@@ -32,18 +32,29 @@
 # 1. Write a function called square that takes a parameter named t, which is a turtle. It
 # should use the turtle to draw a square.
 # Write a function call that passes bob as an argument to square, and then run the program again.
+# import turtle
+# bob = turtle.Turtle()
+# def square(t):
+#     for i in range(4):
+#         t.fd(100)
+#         t.lt(90)
+# square(bob) 
+# turtle.mainloop()
+
+# 2. Add another parameter, named length, to square. 
+# Modify the body so length of the sides is length, and then modify the function call to provide a second argument. 
+# Run the program again. 
+# Test your program with a range of values for length.
+
 import turtle
 bob = turtle.Turtle()
-def square(t):
+def square(t, length):
     for i in range(4):
-        t.fd(100)
+        t.fd(length)
         t.lt(90)
-square(bob) 
+square(bob, -150) 
 turtle.mainloop()
 
-# 2. Add another parameter, named length, to square. Modify the body so length of the
-# sides is length, and then modify the function call to provide a second argument. Run
-# the program again. Test your program with a range of values for length.
 # 3. Make a copy of square and change the name to polygon. Add another parameter
 # named n and modify the body so it draws an n-sided regular polygon. Hint: The
 # exterior angles of an n-sided regular polygon are 360/n degrees.
