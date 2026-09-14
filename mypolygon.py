ca80f9cc2606e3c1fb6921a9bc123df48020c5f7
Nikhil kumar Mettapally <nikhil.mettapally@gmail.com>
2026-09-14 16:21:04 +0200

    Add files via upload

--- mypolygon.py
+++ mypolygon.py
@@ -59,20 +59,38 @@
 # named n and modify the body so it draws an n-sided regular polygon. Hint: The
 # exterior angles of an n-sided regular polygon are 360/n degrees.
 
-import turtle
-bob = turtle.Turtle()
-def polygon(t, length, n):
-    for i in range(n):
-        t.fd(length)
-        t.lt(360/n)
-polygon(bob, 150, 5) 
-turtle.mainloop()
+# import turtle
+# bob = turtle.Turtle()
+# def polygon(t, length, n):
+#     for i in range(n):
+#         t.fd(length)
+#         t.lt(360/n)
+# polygon(bob, 150, 5) 
+# turtle.mainloop()
 
 # 4. Write a function called circle that takes a turtle, t, and radius, r, as parameters and
 # that draws an approximate circle by calling polygon with an appropriate length and
 # number of sides. Test your function with a range of values of r.
 # Hint: figure out the circumference of the circle and make sure that length * n =
 # circumference.
+
+import turtle
+bob = turtle.Turtle()
+def circle(t, r):
+    # Calculate the number of sides for the polygon
+    n = 25
+    # Calculate the length of each side
+    length = 2 * r * 3.14159 / n
+    for i in range(n):
+        t.fd(length)
+        t.lt(360/n)
+circle(bob, 100) 
+turtle.mainloop()
+
 # 5. Make a more general version of circle called arc that takes an additional parameter
 # angle, which determines what fraction of a circle to draw. angle is in units of degrees,
 # so when angle=360, arc should draw a complete circle.
+
+import turtle
+bob = turtle.Turtle()
+def arc(t, r, angle):
