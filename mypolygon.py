8199431562f417612001b04d488d7a4a8ebf3f2e
Nikhil kumar Mettapally <nikhil.mettapally@gmail.com>
2026-09-25 12:33:52 +0200

    Add files via upload

--- mypolygon.py
+++ mypolygon.py
@@ -664,28 +664,53 @@
 #     n = int(input("Enter a value for n: "))
 #     check_fermat(a, b, c, n)
 
-# Exercise 5.3. 
+# # Exercise 5.3. 
 
-# If you are given three sticks, you may or may not be able to arrange them in a triangle.
-# For example, if one of the sticks is 12 inches long and the other two are one inch long, you will not
-# be able to get the short sticks to meet in the middle. For any three lengths, there is a simple test to
-# see if it is possible to form a triangle:
-# If any of the three lengths is greater than the sum of the other two, then you cannot
-# form a triangle. Otherwise, you can. (If the sum of two lengths equals the third, they
-# form what is called a “degenerate” triangle.)
-# 1. Write a function named is_triangle that takes three integers as arguments, and that prints
-# either “Yes” or “No”, depending on whether you can or cannot form a triangle from sticks
-# with the given lengths.
+# # If you are given three sticks, you may or may not be able to arrange them in a triangle.
+# # For example, if one of the sticks is 12 inches long and the other two are one inch long, you will not
+# # be able to get the short sticks to meet in the middle. For any three lengths, there is a simple test to
+# # see if it is possible to form a triangle:
+# # If any of the three lengths is greater than the sum of the other two, then you cannot
+# # form a triangle. Otherwise, you can. (If the sum of two lengths equals the third, they
+# # form what is called a “degenerate” triangle.)
+# # 1. Write a function named is_triangle that takes three integers as arguments, and that prints
+# # either “Yes” or “No”, depending on whether you can or cannot form a triangle from sticks
+# # with the given lengths.
 
-def is_triangle(a, b, c):
-    if a + b > c and a + c > b and b + c > a:
-        print("Yes")
+# def is_triangle(a, b, c):
+#     if a + b > c and a + c > b and b + c > a:
+#         print("Yes")
+#     else:
+#         print("No")
+
+# # 2. Write a function that prompts the user to input three stick lengths, converts them to integers,
+# # and uses is_triangle to check whether sticks with the given lengths can form a triangle.
+# a = int(input("Enter the length of the first stick: "))
+# b = int(input("Enter the length of the second stick: "))
+# c = int(input("Enter the length of the third stick: "))
+# is_triangle(a, b, c)
+
+# Exercise 5.4. 
+
+# What is the output of the following program? Draw a stack diagram that shows the
+# state of the program when it prints the result.
+def recurse(n, s):
+    if n == 0:
+        print(s)
     else:
-        print("No")
+        recurse(n-1, n+s)
 
-# 2. Write a function that prompts the user to input three stick lengths, converts them to integers,
-# and uses is_triangle to check whether sticks with the given lengths can form a triangle.
-a = int(input("Enter the length of the first stick: "))
-b = int(input("Enter the length of the second stick: "))
-c = int(input("Enter the length of the third stick: "))
-is_triangle(a, b, c)
+recurse(3, 0)
+
+# 1. What would happen if you called this function like this: recurse(-1, 0)?
+
+recurse(-1, 0)
+
+# 2. Write a docstring that explains everything someone would need to know in order to use this
+# function (and nothing else).
+
+"""
+This function takes two arguments, n and s, and recursively calls itself with n-1 and n+s until n reaches 0, 
+at which point it prints the value of s.
+"""
+
